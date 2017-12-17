fceee7faff21becd45098422e170631b088bc168
Yongyan Liu <legolyy@tamu.edu>
2017-12-17 17:05:24 -0600

    Color Game Part 2: mouse click color and give alert

--- js/main.js
+++ js/main.js
@@ -8,7 +8,21 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var displayColor = document.getElementById("colorDisplay");
 
-for(var i=1; i < squares.length; i++){
+// displayColor.textContent = pickedColor;
+
+for(var i=0; i < squares.length; i++){
   squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+    // alert(this.style.backgroundColor);
+    if(clickedColor === pickedColor){
+      displayColor.textContent = this.style.backgroundColor;
+      alert("Correct!");
+    } else{
+      alert("Wrong!");
+    }
+  })
 }
